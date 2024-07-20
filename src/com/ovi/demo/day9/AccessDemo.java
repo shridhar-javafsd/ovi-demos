d783a53d66cb7c00959da98a6936867d07c17a0a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-20 14:28:39 +0530

    access

---
+++ src/com/ovi/demo/day9/AccessDemo.java
@@ -0,0 +1,14 @@
+package com.ovi.demo.day9;
+
+public class AccessDemo {
+	
+	public static void main(String[] args) {		
+		int num = 10;
+		System.out.println(num);
+	}
+
+	static void m1() {
+		System.out.println(num);
+	}
+	
+}
